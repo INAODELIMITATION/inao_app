b19b219922177a3088e50be2febb797d8f89727a
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-06-29 08:57:10 +0200

    ajout de codeclimate

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Mise en place d'une application de visualisation cartographique des AOC viticoles.
 projet réalisé avec : 
 ## <center>**NodeJS + ExpressJs + Sequelize**</center>
+[![Maintainability](https://api.codeclimate.com/v1/badges/99bf2c129880e5b38009/maintainability)](https://codeclimate.com/github/INAODELIMITATION/Inao_app/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/99bf2c129880e5b38009/test_coverage)](https://codeclimate.com/github/INAODELIMITATION/Inao_app/test_coverage)
--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
     $('#search').typeahead({
 
-       minLength:3,
-       maxItem: 10,     
+        minLength: 3,
+        maxItem: 10,
         source: function (query, result) {
             $.ajax({
                 url: "http://127.0.0.1:3000/search",
-                data: 'denom=' + query,            
+                data: 'denom=' + query,
                 dataType: "json",
                 type: "POST",
                 success: function (data) {
-                    
+
                     result($.map(data, function (item) {
                         return item.denomination;
                     }));
                 }
             });
         },
-      
+
     });
+    
+});
+$('#search').change(function(){
+    var result = $(this).val()
+    //call your function here
+    bob(result);
 });
 
 
-
+function bob(result) {
+    alert('hi bob, you typed: '+ result);
+}
