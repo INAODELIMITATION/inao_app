972b38fb0566db8be0deb1f118afbd7915ada3a5
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-07-23 10:06:55 +0200

    creation d'une couche en fonction des filtres

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -25,28 +25,6 @@ $(document).ready(function () {
                 dataType: "json",
                 success: function (data) {
                     var data1 = data.filter;
-                    var vectorSource = new ol.source.Vector({
-                        format: new ol.format.Feature({
-                            defaultDataProjection: 'EPSG:2154',
-                            projection: 'EPSG:2154'
-                        }),
-                        projection: 'EPSG:2154',
-                        extent: [-357823.2365, 6037008.6939, 1313632.3628, 7230727.3772],
-                        resolutions: resolutions,
-                        origin: ol.extent.getTopLeft(projectionExtent),
-                    });
-                    var VectorLayer = new ol.layer.Vector({
-                        //source:vectorSource,
-                        projection: 'EPSG:2154',
-                        extent: [-357823.2365, 6037008.6939, 1313632.3628, 7230727.3772],
-                        resolutions: resolutions,
-                        origin: ol.extent.getTopLeft(projectionExtent),
-                        //map:map,
-                        renderMode: 'vector',
-                        opacity: 0.8,
-                        style: styles.red
-                    });
-
                     var filteredFeatures = [];
 
                     for (var i = 0; i < data1.length; i++) {
@@ -57,81 +35,21 @@ $(document).ready(function () {
                                 return new ol.style.Style({});
                             }
                           }
-                        map.addLayer(new ol.layer.VectorTile({
-                            opacity: 0.8,
-                            source: sourceL,
-                            style :simpleStyle
-                        }))
-                        // for (var j = 0; j < features.length; j++) {
-
-                        //     if (String(features[j].get(data1[i].type)).valueOf() == String(data1[i].valeur).valueOf()) {
-                        //         console.log("fois please");
-                        //         //filteredFeatures.push(features[j]);
-                        //         try {
-                        //         //     var iFeature = new ol.Feature(features[j].getGeometry());
-                        //         //     // var iFeature = new ol.Feature({
-                        //         //     //     geometry: new ol.geom.Polygon({
-                        //         //     //         coordinates: features[j].getGeometry().getFlatCoordinates(),
-                        //         //     //         layout:features[j].getGeometry().getType()
-
-                        //         //     //     })
-                        //         //     // });
-                        //         //     iFeature.setGeometryName('polygon');
-                        //         //     iFeature.setId(j);
-                        //         //     var feat = features[j];
-                        //         //     console.log(feat.coordinates);
-                        //         //     iFeature.setStyle(styles.red);
-                        //         //      //iFeature.setGeometry(features[j].getGeometry().getFlatCoordinates());
-                        //         //    console.log(Object.keys(iFeature));
-                        //         //    console.log(features[j].getGeometry().getType());
-
-                        //         //     break;
-
-                        //         //     filteredFeatures.push(iFeature);
-                        //         } catch (e) {
-                        //             console.log(e);
-
-                        //         }
-
-
-                        //     }
-
-                        // }
-
+                          try{
+                            map.addLayer(new ol.layer.VectorTile({
+                                opacity: 0.8,
+                                source: sourceL,
+                                style :simpleStyle
+                            }));
+                            successMessage("ajout de la couche "+data1[0].valeur, "ajout termnié avec succès");
+                          }catch(e){
+                            successMessage("erreur lors de l'ajout de la couche "+data1[0].valeur, "erreur :"+e);
+                          }
+                       
+                      
                         console.log("parcours du tableau terminé");
 
-                        // console.log(Object.keys(features));
-
-
-                        //vectorSource.addFeatures(filteredFeatures[0]);
-
-
-                        /* filteredFeatures.forEach(function (feature) {
-                             var f=0;
-                             vectorSource.addFeatures(feature.getFlatCoordinates({
-                                 id:f,
-                             }))
-                             f++;
-                             //sourcep.addFeature(feature.getGeometry());
-                         });*/
-                        // try {
-
-                        //     vectorSource.addFeatures(filteredFeatures);
-                        //     VectorLayer.setSource(vectorSource);
-                        //     console.log(VectorLayer.getSource().getProjection());
-                        //     //vectorSource.setProjection(proj2154);
-
-
-                        // } catch (e) {
-                        //     console.log(e);
-                        // }
-
-
-                        //console.log(filteredFeatures[1].getGeometry());      
-
-
-                        //filteredFeatures = [];
-
+                       
                     }
 
 
