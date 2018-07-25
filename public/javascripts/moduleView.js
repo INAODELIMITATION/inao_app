6af3649b1a30ab08ebcab9e818db7da7117ad1fb
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-07-25 10:36:01 +0200

    travail sur le module Vue

--- public/javascripts/moduleView.js
+++ public/javascripts/moduleView.js
@@ -1,4 +1,11 @@
 /**
  * @author Guiala Jean Roger
  * @module View toutes les fonctions d'interaction et de création de vue
+ * Dans ce fichier nous mettrons toutes les intéractions avec notre vue, création de block etc...
  */
+/**Fonction qui crée une div en fonction de la couche qui est chargée
+ * @param {Object} data la couche a afficher: ces données
+ */
+function createLayerRow(data){
+console.log(data);
+}
