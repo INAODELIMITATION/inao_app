67574c3e4fa26a2521ef0e6188dcdf5b10511fe4
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-08-10 11:51:25 +0200

    travail sur la fonction removeDuplicate

--- public/javascripts/modulesplugins.js
+++ public/javascripts/modulesplugins.js
@@ -13,17 +13,11 @@ function removeDuplicates(arr, key) {
     if (!(arr instanceof Array) || key && typeof key !== 'string') {
         return false;
     }
-
     if (key && typeof key === 'string') {
         return arr.filter((obj, index, arr) => {
             return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === index;
         });
-
-    } else {
-        return arr.filter(function (item, index, arr) {
-            return arr.indexOf(item) == index;
-        });
-    }
+    } 
 }
 
 /**
