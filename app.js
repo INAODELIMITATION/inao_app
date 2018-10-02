ea34e4a3f4859e0c8a9b3e7309076348ea45ca7e
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-10-02 15:25:40 +0200

    max age de la mise en cache

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge:31557600}));
+
 app.use(session({
   secret:'inaoMontpel',
   resave:true,
