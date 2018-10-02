e3e8f63d86256950728e81e29beb4f0fa1e44e96
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-10-02 15:11:49 +0200

    minify

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var compression = require('compression');
+var minify = require('express-minify');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 app.use(compression());
+app.use(minify());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret:'inaoMontpel',
@@ -28,7 +31,7 @@ app.use(session({
   }
 
 }));
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: '30 days' }));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
