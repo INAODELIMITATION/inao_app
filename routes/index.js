2d1995f9e13f73b1426de8b744fa869e40752558
INAODELIM <39952867+INAODELIM@users.noreply.github.com>
2018-10-26 11:17:19 +0200

    ajout des commentaires dans la route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/**
+ * @file Fichier des routes de l'application
+ * @copyright INAO 2018
+ * @author Jean Roger NIGOUMI Guiala <mail@jrking-dev.com>
+ * @version 1.0.0
+ */
+
+/**déclaration des varibales */
 var express     = require('express');
 const  fs       = require('fs');
 const csv       = require("csvtojson");
@@ -16,7 +24,14 @@ const requestController   = require('../controllers').Requests; //controleur pou
 
 // https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 
-
+/**
+ * @author Jean Roger NIGOUMI Guiala <mail@jrking-dev.com>
+ * @function sessionchecker
+ * @description si l'utilisateur est authentifié, affiche la page principale
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 var sessionchecker = (req, res, next) => {
   if (req.session.user && req.cookies.user_sid) {
     res.render('index');
@@ -25,6 +40,14 @@ var sessionchecker = (req, res, next) => {
   }
 };
 
+/**
+ * @function checklogin
+ * @author Jean Roger NIGOUMI Guiala <mail@jrking-dev.com>
+ * @description si l'utilisateur n'est pas authentifié, affiche la page de connexion
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 var checklogin = (req,res, next)=>{
   if(!req.session.user && !req.cookies.user_id){
    
@@ -33,6 +56,15 @@ var checklogin = (req,res, next)=>{
     next();
   }
 };
+
+/**
+ * @function checkloginAdmin
+ * @author Jean Roger NIGOUMI Guiala <mail@jrking-dev.com>
+ * @description si l'utilisateur est pas l'admin, retour à la page principale
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 var checkloginAdmin = (req,res, next)=>{
   if(req.session.user.login !== "j.nigoumiguiala@inao.gouv.fr"){
    
@@ -48,57 +80,68 @@ router.get('/', sessionchecker, function (req, res) {
   res.redirect('/login');
 });
 
-
+/**Page de connexion */
 router.route('/login/:login?')
+/**Get page de connexion, affiche la vue  */
   .get(sessionchecker, (req, res) => {
 
     res.render('login', { log: req.params.login });
   })
+  /**Post oage de connexion HTTP POST du formulaire de connexion */
   .post(userController.login);
 
 
 
-
+/**route pour la recherche (formulaire de recherche des appellations) */
 router.route('/search')
   .post(checklogin,lbl_AireController.findLibelle);
 
+  /**route pour recupérer l'aire géo */
 router.route('/zone/aire_geo/:id_aire')
   .get(checklogin,zonesController.getAire);
-
+  
+/**recupère toute l'aire géo */
 router.route('/aire_geo/:id_aire')
   .get(checklogin,zonesController.findAireGeo);
 
+  /**recupère l'aire parcellaire */
 router.route('/zone/aire_parcellaire/:id_aire')
   .get(checklogin,zonesController.getParcellaire);
 
 
 
-
+/**extend de l'aire parcellaire */
 router.get('/getExtendParcellaire/:id_aire',checklogin, zonesController.getExtend);
 
+/**recupere la liste des  communes recherchées */
 router.route('/communes')
   .post(checklogin,communesController.fetchCommunes);
 
+/**recupère la commune qu'on veut afficher */
 router.route('/communes/:insee')
   .get(checklogin,communesController.getCommune);
 
+/**liste des parcelles */
 router.route('/parcelles')
   .post(checklogin,parcellesController.fetchParcelles);
 
+/**parcelle qu'on veut afficher */
 router.route('/parcelles/:id')
   .get(checklogin,parcellesController.getParcelle);
 
-
+/**listes des appellations qui touchent des coordonnées  */
 router.route('/listAppel/:x/:y')
   .get(zonesController.listAppellationOncoord);
 
-
+/**lien vers les cahiers des charges */
 router.route('/siqo/lien/:id_aire')
 .get(checklogin,lienSiqoController.getLien);
 
+/**enregistre une ligne dans la table requete quand une appellation est cherchée */
 router.route('/request/:id_aire')
 .get(checklogin,requestController.createRequest);
 
+/**upload csv */
 router.route('/csv/upload')
 .get((req,res)=>{
   res.render("upload", {statue:"noform"});
