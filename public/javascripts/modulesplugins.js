a9fdadb8aee01169f23682f253fd06377811e64a
BAUSSIER Martin <M.BAUSSIER@inao.gouv.fr>
2022-02-06 00:43:28 +0100

    test wgs84 11

--- public/javascripts/modulesplugins.js
+++ public/javascripts/modulesplugins.js
@@ -37,8 +37,8 @@ function initialisation() {
  
     try {
         createOSM("opensmap", "OpenstreetMap", false, 0.7);
-        setIgnLayer('https://wxs.ign.fr/cartes/geoportail/wmts','GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2', 0.7, 'plan IGN', false);
-        setIgnLayer('https://wxs.ign.fr/parcellaire/geoportail/wmts','CADASTRALPARCELS.PARCELLAIRE_EXPRESS', 0.7, 'PCI vecteur', false);
+        //setIgnLayer('https://wxs.ign.fr/cartes/geoportail/wmts','GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2', 0.7, 'plan IGN', false);
+        //setIgnLayer('https://wxs.ign.fr/parcellaire/geoportail/wmts','CADASTRALPARCELS.PARCELLAIRE_EXPRESS', 0.7, 'PCI vecteur', false);
         setIgnLayer('https://wxs.ign.fr/ortho/geoportail/wmts','HR.ORTHOIMAGERY.ORTHOPHOTOS', 0.7, 'ortho-photo 20cm', false);
 
     } catch (error) {
