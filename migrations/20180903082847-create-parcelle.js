0566c223c7b99618e5df8e909efa63037ed2e384
martin baussier <martin.baussier@gmail.com>
2024-04-23 11:46:58 +0200

    Reapply "test modif migration t_parcelle to parcelle"
    This reverts commit 46d1dba00731ceae82fd4eebfbdb811c88169504.

--- migrations/20180903082847-create-parcelle.js
+++ migrations/20180903082847-create-parcelle.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('t_parcelle', {
+    return queryInterface.createTable('parcelle', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('t_parcelle');
+    return queryInterface.dropTable('parcelle');
   }
 };
