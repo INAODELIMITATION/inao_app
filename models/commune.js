dfef94766b94c7d15650769e7d25cb1c4eb566d1
BDDC <m.baussier@inao.gouv.fr>
2025-11-18 11:41:22 +0100

    Update commune.js
    Test d'un autre modèle

--- models/commune.js
+++ models/commune.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Commune = sequelize.define('commune', {
-    nom_com: DataTypes.STRING,
-    code_insee: DataTypes.STRING,
+  var Commune = sequelize.define('ign_adminexpress_commune_2025', {
+    nom_officiel: DataTypes.STRING(46),
+    code_insee: DataTypes.STRING(5),
     geom: DataTypes.GEOMETRY('MULTIPOLYGON',2154)
   }, 
   {
     timestamps: false, //n'ajoute pas les attributs createdAt et updatedAt
     freezeTableName: true, // n'ajoute pas de s au nom de la table
-    schema:'ign_bd_cadastre',
+    schema:'new_data',
   });
   Commune.associate = function(models) {
     // associations can be defined here
